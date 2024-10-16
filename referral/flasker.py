932f9d49f7c280e854e551ea88c0aa159f238d77
Tryd0g0lik <work80@mail.ru>
2024-10-16 15:15:40 +0700

    [master]: update

--- referral/flasker.py
+++ referral/flasker.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_jwt_extended import (JWTManager,
-                                create_access_token)
-
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
 jwt = JWTManager(app)
