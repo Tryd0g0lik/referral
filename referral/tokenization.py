06d9c3909b9b8dad7cb7e45bd5647b7fd2ca2274
Tryd0g0lik <work80@mail.ru>
2024-10-17 07:11:23 +0700

    [master]: 'tokenization.py' was added

---
+++ referral/tokenization.py
@@ -0,0 +1,15 @@
+"""Here a token generater """
+import uuid
+tokens = set()
+
+def generate_unique_token() -> str:
+    """
+    Here a token generate. It a unique token returning.
+    :return: str Exemple '3beeb1f1-2ec2-475e-bd38-4952f2e4235b'
+    """
+    while True:
+        new_token = str(uuid.uuid4())
+        if new_token not in tokens:
+            tokens.add(new_token)
+            return new_token
+
