1d10d027968ba99573bb3d38f0393b2a6ca06c26
Tryd0g0lik <work80@mail.ru>
2024-10-23 11:45:28 +0700

    [master]:  clear/update  the referral/tokenization.py

--- referral/tokenization.py
+++ referral/tokenization.py
@@ -32,15 +32,25 @@ def generate_unique_referral_code(func: Callable[[], str]) -> str:
     return hashed_code.decode("utf-8")
 
 class EmailToGenerateToken:
+    """
+    This class is a token's generate.
+    :param app: object from 'app = Flask(__name__)' for a entrypoint
+    """
     def __init__(self, app: type(app_)):
         self.__s = URLSafeTimedSerializer(app.secret_key)
    
     def generate_dumps_token(self, email: str) -> str:
+        """
+        This is token's generate. In token inserting email.
+        :param email: str. Is an user's email.
+        :return: token: str
+        """
         return self.__s.dumps(email, salt="email-confirm")
     
     def set_load_token(self, token: str) -> None:
         """
-        Token receive to an event point
+        This a method is addition for 'get_load_token'.
+        Token receive and data to transform for the private variable.
         :param token: str
         :return:
         """
@@ -49,9 +59,8 @@ class EmailToGenerateToken:
      
     def get_load_token(self) -> str:
         """
-        
-        :return: 'email': str
-        
+        This a method is addition for 'set_load_token'
+        :return: 'email': str If a time of create is more 120 sec return error.
         """
         token = ''
         token += self.__s.loads(
